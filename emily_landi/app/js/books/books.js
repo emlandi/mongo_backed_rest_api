97a656ad1fd27356a21226dee2f1abb47e74d62f
Emily Landi <emily.landi@gmail.com>
2015-12-10 10:50:08 -0800

    file setup for refactor assignment

--- emily_landi/app/js/books/books.js
+++ emily_landi/app/js/books/books.js
@@ -1,3 +1,5 @@
 module.exports = function(app) {
   require('./controllers/books_controller')(app);
+  require('./directives/book_directive')(app);
+  require('./directives/book_transclude_directive')(app);
 };
--- emily_landi/app/js/entry.js
+++ emily_landi/app/js/entry.js
@@ -4,4 +4,5 @@ var angular = window.angular;
 var bookstoreApp = angular.module('BookstoreApp', []);
 require('./services/services')(bookstoreApp);
 require('./controllers/controllers')(bookstoreApp);
+require('./directives/directives')(bookstoreApp);
 require('./books/books')(bookstoreApp);
